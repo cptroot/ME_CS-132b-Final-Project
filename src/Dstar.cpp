fb20111056be3c4cd65dff75b881d8f581c33251
Sith Domrongkitchaiporn <sdomrong@caltech.edu>
2017-06-01 16:36:54 -0700

    Add more things to hpp file

--- src/Dstar.cpp
+++ src/Dstar.cpp
@@ -4,11 +4,17 @@
 Dstar::Dstar() {
     int world_x;
     int world_y;
-    
+    cell* world;
     int start;
     int goal;
     int size;
     std::priority_queue<cell> pqueue;
+    
+    world = new cell[world_x * world_y]
+}
+
+Dstar::~Dstar() {
+    delete world;
 }
 
 cell Dstar::get(n) {
--- src/Dstar.hpp
+++ src/Dstar.hpp
@@ -1,21 +1,56 @@
 
-struct cell {
-    float h;
-    float k;
-    float b;
-    char t;
+struct coordinate {
+    int x;
+    int y;
     
-    cell(float a, float b, float c, char d) : h(a), k(b), b(c), t(d) {}    
+    coordinate(int a, int b) : x(a), y(b) {}
+};
+
+// Access cell values:
+//   h:      path cost
+//   k:      smallest value of h seen so far
+//   b:      back pointer
+//   t:      tag ('c' - closed; 'o' - open; 'n' - new)
+struct cell {
+    double h;
+    double k;
+    double b;
+    char t = 'n';
+    coordinate loc;
+    cell(double a, double b, double c, double d, coordinate e) : h(a), k(b), b(c), t(d), loc(e) {}    
 };
 
 class Dstar() {
     public:
         Dstar();
         ~Dstar();
-        cell get();
+        cell get(coordinate n);
+        cell put(coordinate n, cell item);
+        // auto get_open();  
+        void put_open(cell);
+        // get_kmin():
+        
+       get_cost(cell curr1, cell curr2);
+       std::vector<coordinate> get_path(cell curr);
+       std::vector<coordinate> get_neighhors(cell curr);
+       
+       /*************************************/
+       
+       std::vector<coordinate> init_path();
+       void change_map(octomap::AbstractOcTree*, cell curr);
+       std::vector<coordinate> navigate_map(cell curr);
+       // depends on get_kmin // modify_costs(cell curr1, cell curr2, float new_cost);
+       void insert(cell curr, double new_h);
+       // depends on get_kmin // process_state();
         
     private:
-    
+        int world_x;
+        int world_y;
+        cell* world;
+        int start;
+        int goal;
+        int size;
+        std::priority_queue<cell> pqueue;
 };
 
 
